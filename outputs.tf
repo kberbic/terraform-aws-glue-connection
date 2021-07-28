896d721c15d454a484886c51a12d79e4bc5b355a
Kenan Berbic <kenan.berbic@symphony.is>
2021-07-28 11:42:22 +0200

    Add name in outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "id" {
   value = "${aws_glue_connection.glue_connection.id}"
 }
 
+output "name" {
+  value = "${aws_glue_connection.glue_connection.name}"
+}
+
 output "arn" {
   value = "${aws_glue_connection.glue_connection.arn}"
 }
