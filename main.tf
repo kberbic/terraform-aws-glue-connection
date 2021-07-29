f90023fb9450e28253f8c863934b38592d6d306b
Kenan Berbic <kenan.berbic@symphony.is>
2021-07-29 16:12:55 +0200

    Add support for jdb

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_glue_connection" "glue_connection" {
   connection_properties = {
-    JDBC_CONNECTION_URL = "jdbc://${var.jdbc}/${var.database}"
+    JDBC_CONNECTION_URL = "jdbc:postgresql://${var.jdbc}/${var.database}"
     PASSWORD            = var.password
     USERNAME            = var.username
   }
